3102368aea13ae03fa347444ef102608a0fe28f2
Benjamin Coullie <ben.coullie@vendhq.com>
2020-12-23 11:36:59 +1300

    tidy up html + improve css

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,30 @@
 <script lang="ts">
-	import ChessBoard from './chessBoard/chessBoard.svelte';
+  import ChessBoard from "./chessBoard/chessBoard.svelte";
 </script>
 
 <style>
   main {
+    font-family: "Lato", sans-serif;
+    font-weight: 300;
     text-align: center;
     padding: 1em;
     max-width: 240px;
-		margin: 0 auto;
+    margin: 0 auto;
   }
 
   h1 {
-    color: #ff3e00;
+    margin-top: 0;
+    margin-bottom: 26px;
+    font-weight: 100;
+    color: #fcc9b9;
     text-transform: uppercase;
     font-size: 4em;
     font-weight: 100;
+  }
+
+  span {
+    font-weight: 900;
+    color: #ccc;
   }
 
   @media (min-width: 640px) {
@@ -25,6 +35,6 @@
 </style>
 
 <main>
-  <h1>Svelte Chess!</h1>
+  <h1>Svelte <span>Chess</span></h1>
   <ChessBoard />
 </main>
