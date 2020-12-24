fdabbee018af8c66a8209214010caac9513cf673
Benjamin Coullie <ben.coullie@vendhq.com>
2020-12-24 16:57:23 +1300

    improve warning message

--- src/utilities/alertBadMove.ts
+++ src/utilities/alertBadMove.ts
@@ -2,9 +2,9 @@ import Toastify from 'toastify-js'
 
 const alertBadMove = () => {
   const defaults = {
-    text: 'Nope, can\t do that.',
-    gravity: "top", // `top` or `bottom`
-    position: "center", // `left`, `center` or `right`
+    text: 'Illegal move: your king would be in check.',
+    gravity: "top",
+    position: "center",
     backgroundColor: '#f87d58',
     stopOnFocus: true, // Prevents dismissing of toast on hover
   }
